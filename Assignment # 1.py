17ed4676a823722a6351b83d700fa3ad70fb6b9b
Ahmedmalik99 <ahmedabidjawad@gmail.com>
2024-01-17 02:31:09 +0500

    Initial commit

---
+++ Assignment # 1.py
@@ -0,0 +1,23 @@
+# list= (15, 1, 45, 12, 7)
+#
+# v1=list[1]
+#
+# print(v1)
+
+
+# assigment 2
+
+# n1 =int(input("enter a number"))
+#
+# result = ({n1} / 2)
+#
+# r=type(result)
+# print (r)
+
+# assigment 3
+
+arr =  int("90 100 78 89 67")
+
+if arr[4] < [0] :
+    print ([4])
+
