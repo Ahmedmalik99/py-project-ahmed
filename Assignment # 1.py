e5ee4d4e3cee101a2d17405b67a1653b782c2e92
Ahmedmalik99 <ahmedabidjawad@gmail.com>
2024-01-18 03:22:52 +0500

    Initial file

--- Assignment # 1.py
+++ Assignment # 1.py
@@ -1,3 +1,34 @@
+# assigment 1
+
+# q = [5, 4, 2, 7]
+#
+# a = int(input("enter a number: "))
+# b = int(input("enter a number: "))
+# c = int(input("enter a number: "))
+# d = int(input("enter a number: "))
+#
+# if q[0] == a:
+#     print("index", q[0], "=", a)
+#
+# else:
+#    print ("index does not match")
+#
+# if q[1] == b:
+#     print("index", q[1], "=", b)
+# else:
+#    print ("index does not match")
+#
+# if q[2] == c:
+#     print("index", q[2], "=", c)
+#
+# else:
+#     print ("index does not match")
+#
+# if q[3] == d:
+#     print("index", q[3], "=", d)
+#
+# else:
+#     print("index does not match")
 
 
 # assigment 2
@@ -27,114 +58,114 @@
 
 # for num1
 
-arr = [90, 100, 78, 89, 67]
-
-a = []
-
-if arr[4] > arr[0]:
-    print(arr[0])
-
-elif arr[4] > arr[1]:
-    print(arr[1])
-
-
-elif arr[4] > arr[2]:
-    print(arr[2])
-
-elif arr[4] > arr[3]:
-    print(arr[3])
-
-else:
-    a.append(arr[4])
-
-
-# for num2
-
-if arr[2] > arr[0]:
-    print(arr[0])
-
-elif arr[2] > arr[1]:
-    print(arr[1])
-
-
-elif arr[2] < arr[4]:
-    print(arr[4])
-
-elif arr[2] > arr[3]:
-    print(arr[3])
-
-else:
-    a.append(arr[2])
-
-# for num3
-
-if arr[3] < arr[3]:
-    print(arr[3])
-
-elif arr[3] > arr[1]:
-    print(arr[1])
-
-
-elif arr[3] < arr[2]:
-    print(arr[2])
-
-elif arr[3] < arr[4]:
-    print(arr[4])
-
-else:
-    a.append(arr[3])
-
-
-# for num4
-
-if arr[0] < arr[3]:
-    print(arr[3])
-
-elif arr[0] > arr[1]:
-    print(arr[1])
-
-
-elif arr[0] < arr[2]:
-    print(arr[2])
-
-elif arr[0] < arr[4]:
-    print(arr[4])
-
-else:
-    a.append(arr[0])
-
-# for num5
-
-if arr[1] < arr[3]:
-    print(arr[3])
-
-elif arr[1] > arr[1]:
-    print(arr[1])
-
-
-elif arr[1] < arr[0]:
-    print(arr[2])
-
-elif arr[1] < arr[4]:
-    print(arr[4])
-
-else:
-    a.append(arr[1])
-
-print(a)
-
-
-len1 = len(a)
-
-print( "lenth of a sorted list is", len1,"so the lenth of a sorted list is odd so the formula for it will be n+1/2")
-
-print (a)
-print("n+1/2")
-len3 = (len1 + 1, "/2")
-print (len3)
-len3 = (len1 + 1)
-
-result = int(len3 / 2)
-print (result)
-
-print("the",result,"value in sorted list is" ,a[2])
+# arr = [90, 100, 78, 89, 67]
+#
+# a = []
+#
+# if arr[4] > arr[0]:
+#     print(arr[0])
+#
+# elif arr[4] > arr[1]:
+#     print(arr[1])
+#
+#
+# elif arr[4] > arr[2]:
+#     print(arr[2])
+#
+# elif arr[4] > arr[3]:
+#     print(arr[3])
+#
+# else:
+#     a.append(arr[4])
+#
+#
+# # for num2
+#
+# if arr[2] > arr[0]:
+#     print(arr[0])
+#
+# elif arr[2] > arr[1]:
+#     print(arr[1])
+#
+#
+# elif arr[2] < arr[4]:
+#     print(arr[4])
+#
+# elif arr[2] > arr[3]:
+#     print(arr[3])
+#
+# else:
+#     a.append(arr[2])
+#
+# # for num3
+#
+# if arr[3] < arr[3]:
+#     print(arr[3])
+#
+# elif arr[3] > arr[1]:
+#     print(arr[1])
+#
+#
+# elif arr[3] < arr[2]:
+#     print(arr[2])
+#
+# elif arr[3] < arr[4]:
+#     print(arr[4])
+#
+# else:
+#     a.append(arr[3])
+#
+#
+# # for num4
+#
+# if arr[0] < arr[3]:
+#     print(arr[3])
+#
+# elif arr[0] > arr[1]:
+#     print(arr[1])
+#
+#
+# elif arr[0] < arr[2]:
+#     print(arr[2])
+#
+# elif arr[0] < arr[4]:
+#     print(arr[4])
+#
+# else:
+#     a.append(arr[0])
+#
+# # for num5
+#
+# if arr[1] < arr[3]:
+#     print(arr[3])
+#
+# elif arr[1] > arr[1]:
+#     print(arr[1])
+#
+#
+# elif arr[1] < arr[0]:
+#     print(arr[2])
+#
+# elif arr[1] < arr[4]:
+#     print(arr[4])
+#
+# else:
+#     a.append(arr[1])
+#
+# print(a)
+#
+#
+# len1 = len(a)
+#
+# print( "lenth of a sorted list is", len1,"so the lenth of a sorted list is odd so the formula for it will be n+1/2")
+#
+# print (a)
+# print("n+1/2")
+# len3 = (len1 + 1, "/2")
+# print (len3)
+# len3 = (len1 + 1)
+#
+# result = int(len3 / 2)
+# print (result)
+#
+# print("the",result,"value in sorted list is" ,a[2])
