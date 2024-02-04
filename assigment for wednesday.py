7c1d00ff43dca3b9b57feb21e2f47310e49e5902
Ahmedmalik99 <ahmedabidjawad@gmail.com>
2024-02-04 15:28:41 +0500

    initiaL

--- assigment for wednesday.py
+++ assigment for wednesday.py
@@ -111,15 +111,15 @@ a = 5
 
 # assigment for palindrome
 
-def k(pal):
-    if pal == pal[::-1]:
-        return "it is palindrome"
-    else:
-        return "it is not palindrome "
-
-x = input("Enter: ")
-out =k(x)
-print(out)
+# def k(pal):
+#     if pal == pal[::-1]:
+#         return "it is palindrome"
+#     else:
+#         return "it is not palindrome "
+#
+# x = input("Enter: ")
+# out =k(x)
+# print(out)
 
 
 # assigment to find vowels
@@ -149,3 +149,77 @@ print(out)
 # a = int(input("Enter any number: "))
 # out = k(a)
 # print(f"{a} is divisible by {out}")
+
+
+
+# assigment
+
+#
+# 1 : Create a list of words
+# word_list = ["ahmed", "kazim", "abdullah", "ayan"]
+#
+#  2: Create a string from the list
+# word_string = ' '.join(word_list)
+#
+# 3 : Capitalize the first letter of the entire string
+# capitalized_string = word_string.capitalize()
+#
+#  4 : Find and print the index of the first occurrence of the letter 'a' in the string
+# index_of_a = capitalized_string.find('a')
+# print(f"Index of 'a': {index_of_a}")
+#
+# 5 : Count and print the number of occurrences of the letter 'a'
+# count_of_a = capitalized_string.count('a')
+# print(f"Number of occurrences of 'a': {count_of_a}")
+#
+#  6 : Replace 'a' with 'x'. Print the modified string
+# modified_string = capitalized_string.replace('a', 'b')
+# print(f"Modified string after replacing 'a' with 'x': {modified_string}")
+#
+# 7 : Remove leading and trailing whitespaces. Print the modified string
+# trimmed_string = modified_string.strip()
+# print(f" removing leading whitespaces: {trimmed_string}")
+#
+# 8 : Split the string back into a list. Print the resulting list
+# resulting_list = trimmed_string.split()
+# print(f"Print the resulting list: {resulting_list}")
+
+
+
+# assigment
+
+
+# Ask the user to input a username
+# user_username = input("Enter your username: ")
+
+def name(username):
+    if len(username) >= 8:
+        if not username[0].isdigit():
+            print("Username is valid.")
+        else:
+            print("Username must not start with a number.")
+    else:
+        print("Username must have at least 8 characters.")
+
+# Take input as 'a'
+a = input("Enter your username: ")
+name(a)
+
+
+
+
+# assigment for pasword
+
+# a = input("Enter your password: ")
+#
+# special_characters = "!@#$%^&*()-_=+[]{}|;:'\",.<>?`~"
+#
+# for i in a:
+#     if i in special_characters:
+#         if len(a) >= 8 and (i.isupper() for i in a):
+#             print("Password strength: Strong")
+#         else:
+#             print("Password strength: Weak")
+#             print("Password should be at least 8 characters long and contain at least one uppercase letter.")
+
+
